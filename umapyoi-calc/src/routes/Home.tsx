afdf821eedc80932ebabf75e07419c1d0f5693d3
Tenshi . <jrcontreras446@gmail.com>
2026-08-18 18:11:16 -0600

    last commit on main, will start working with tickets

--- umapyoi-calc/src/routes/Home.tsx
+++ umapyoi-calc/src/routes/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
       <div className="flex flex-col max-w-3xl gap-5 relative z-10">
         <h1 className="text-5xl md:text-6xl font-bold text-gray-300 text-center mt-20">Umapyoi Calculator</h1>
         <p className="text-base md:text-lg font-bold text-gray-300 text-center">
-          Umamusume Database and Resources Tool for Global and JP server
+          Umamusume Database and Resources Tool for Global and JP server -TEST SERVER-
         </p>
       </div>
     </div>
